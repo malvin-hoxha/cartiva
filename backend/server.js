2caba8a1db56a67544fbd82d92a7f18531063f61
malvin-hoxha <malvinhodev@gmail.com>
2026-09-01 17:05:42 +0300

    feat: add auth session routes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from "express"
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
+
 import authRoutes from "./routes/auth.route.js"
+import productRoutes from "./routes/product.route.js"
 import {connectDB} from "./lib/db.js"
 
 dotenv.config();
@@ -10,8 +13,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
