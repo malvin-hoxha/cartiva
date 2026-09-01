535b5485e09150238f7e0e4c9d86aaaed6beaac7
malvin-hoxha <malvinhodev@gmail.com>
2026-09-01 15:56:24 +0300

    feat: add signup authentication flow

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,3 +1,63 @@
+import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+
+const generateTokens = (userId) => {
+    const accessToken = jwt.sign({userId}, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "15m"
+    });
+
+    const refreshToken = jwt.sign({userId}, process.env.REFRESH_TOKEN_SECRET, {
+        expiresIn: "7d"
+    });
+
+    return { accessToken, refreshToken };
+}
+
+const storeRefreshToken = async (userId, refreshToken) => {
+	await redis.set(`refresh_token:${userId}`, refreshToken, "EX", 7 * 24 * 60 * 60); // 7d
+};
+
+const setCookies = (res, accessToken, refreshToken) => {
+    res.cookie("accessToken", accessToken, {
+        httpOnly: true,
+        sameSite: "strict",
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 15 * 60 * 1000 //15m
+    });
+
+    res.cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        sameSite: "strict",
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 7 * 24 * 60 * 60 * 1000 //7d
+    });
+
+}
+
 export const signup = async (req, res) => {
-    res.send("signup");
+    try {
+        const { name, email, password } = req.body;
+
+        const userExists = await User.findOne({ email });
+
+        if(userExists) {
+            return res.status(400).json({message: "User already exists"});
+        }
+
+        const user = await User.create({ name, email, password });
+
+        const { accessToken, refreshToken } = generateTokens(user._id);
+        await storeRefreshToken(user._id, refreshToken);
+
+        setCookies(res, accessToken, refreshToken);
+
+        res.status(201).json({user: {
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role
+        }, message: "User created successfully"});
+    } catch (error) {
+        res.status(500).json({message: error.message}); 
+    }
 }
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema(
 	{
@@ -40,6 +41,21 @@ const userSchema = new mongoose.Schema(
 		timestamps: true,
 	}
 );
+
+//pre-save hook to hash password before saving to db
+userSchema.pre("save", async function (next) {
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
+userSchema.methods.comparePassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+}
 
 const User = mongoose.model("User", userSchema);
 
